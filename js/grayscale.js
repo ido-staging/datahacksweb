b55f98b924cbd9f97c82f87a5ea0be7ada074a19
idoivri <idoivri@gmail.com>
2015-10-10 10:50:22 +0300

    add gif and layouts

--- js/grayscale.js
+++ js/grayscale.js
@@ -16,10 +16,11 @@ $(window).scroll(function() {
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
+        console.log("scrolling!");
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        },  1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
